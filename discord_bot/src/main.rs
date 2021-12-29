1f9d452ba7656fcf39c60dc5bbe2755d2a2cd5a5
Scott Kirkpatrick <scottkirk@scottkirk.dev>
2021-12-29 15:52:43 -0500

    Add basic discord commands

--- discord_bot/src/main.rs
+++ discord_bot/src/main.rs
@@ -1,3 +1,259 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    fmt::Write,
+    sync::Arc,
+};
+
+use serenity::prelude::*;
+use serenity::{
+    async_trait,
+    client::bridge::gateway::{GatewayIntents, ShardId, ShardManager},
+    framework::standard::{
+        help_commands,
+        macros::{command, group, help, hook},
+        Args,
+        CommandGroup,
+        CommandResult,
+        DispatchError,
+        HelpOptions,
+        StandardFramework,
+    },
+    http::Http,
+    model::{
+        channel::{Message},
+        gateway::Ready,
+        id::UserId,
+    },
+};
+use tokio::sync::Mutex;
+
+// A container type is created for inserting into the Client's `data`, which
+// allows for data to be accessible across all events and framework commands, or
+// anywhere else that has a copy of the `data` Arc.
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct CommandCounter;
+
+impl TypeMapKey for CommandCounter {
+    type Value = HashMap<String, u64>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[group]
+#[commands(status)]
+struct General;
+
+#[group]
+#[owners_only]
+#[summary = "Commands for server owners"]
+#[commands(commands, latency, startup, shutdown)]
+struct Owner;
+
+#[help]
+#[individual_command_tip = "Hello! こんにちは！Hola! Bonjour! 您好! 안녕하세요~\n\n\
+If you want more information about a specific command, just pass the command as argument."]
+#[command_not_found_text = "Could not find: `{}`."]
+#[max_levenshtein_distance(3)]
+#[indention_prefix = "+"]
+#[lacking_permissions = "Hide"]
+#[lacking_role = "Nothing"]
+#[wrong_channel = "Strike"]
+async fn my_help(
+    context: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
+
+#[hook]
+async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
+    println!("Got command '{}' by user '{}'", command_name, msg.author.name);
+
+    let mut data = ctx.data.write().await;
+    let counter = data.get_mut::<CommandCounter>().expect("Expected CommandCounter in TypeMap.");
+    let entry = counter.entry(command_name.to_string()).or_insert(0);
+    *entry += 1;
+
+    true
+}
+
+#[hook]
+async fn after(_ctx: &Context, _msg: &Message, command_name: &str, command_result: CommandResult) {
+    match command_result {
+        Ok(()) => println!("Processed command '{}'", command_name),
+        Err(why) => println!("Command '{}' returned error {:?}", command_name, why),
+    }
+}
+
+#[hook]
+async fn unknown_command(_ctx: &Context, _msg: &Message, unknown_command_name: &str) {
+    println!("Could not find command named '{}'", unknown_command_name);
+}
+
+#[hook]
+async fn normal_message(_ctx: &Context, msg: &Message) {
+    println!("Message is not a command '{}'", msg.content);
+}
+
+#[hook]
+async fn delay_action(ctx: &Context, msg: &Message) {
+    let _ = msg.react(ctx, '⏱').await;
+}
+
+#[hook]
+async fn dispatch_error(ctx: &Context, msg: &Message, error: DispatchError) {
+    if let DispatchError::Ratelimited(info) = error {
+        if info.is_first_try {
+            let _ = msg
+                .channel_id
+                .say(&ctx.http, &format!("Try this again in {} seconds.", info.as_secs()))
+                .await;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let http = Http::new_with_token(&token);
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c
+            .with_whitespace(true)
+            .on_mention(Some(bot_id))
+            .prefix("~")
+            .delimiters(vec![", ", ","])
+            .owners(owners))
+        .before(before)
+        .after(after)
+        .unrecognised_command(unknown_command)
+        .normal_message(normal_message)
+        .on_dispatch_error(dispatch_error)
+        .help(&MY_HELP)
+        .group(&GENERAL_GROUP)
+        .group(&OWNER_GROUP);
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .intents(GatewayIntents::all())
+        .type_map_insert::<CommandCounter>(HashMap::default())
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+    }
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
+
+#[command]
+async fn status(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Status checks are not yet available").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn latency(ctx: &Context, msg: &Message) -> CommandResult {
+    // The shard manager is an interface for mutating, stopping, restarting, and
+    // retrieving information about shards.
+    let data = ctx.data.read().await;
+
+    let shard_manager = match data.get::<ShardManagerContainer>() {
+        Some(v) => v,
+        None => {
+            msg.reply(ctx, "There was a problem getting the shard manager").await?;
+
+            return Ok(());
+        },
+    };
+
+    let manager = shard_manager.lock().await;
+    let runners = manager.runners.lock().await;
+
+    // Shards are backed by a "shard runner" responsible for processing events
+    // over the shard, so we'll get the information about the shard runner for
+    // the shard this command was sent over.
+    let runner = match runners.get(&ShardId(ctx.shard_id)) {
+        Some(runner) => runner,
+        None => {
+            msg.reply(ctx, "No shard found").await?;
+
+            return Ok(());
+        },
+    };
+
+    msg.reply(ctx, &format!("The shard latency is {:?}", runner.latency)).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn commands(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut contents = "Commands used:\n".to_string();
+
+    let data = ctx.data.read().await;
+    let counter = data.get::<CommandCounter>().expect("Expected CommandCounter in TypeMap.");
+
+    for (k, v) in counter {
+        writeln!(contents, "- {name}: {amount}", name = k, amount = v)?;
+    }
+
+    msg.channel_id.say(&ctx.http, &contents).await?;
+
+    Ok(())
+}
+
+#[command]
+#[aliases("start")]
+async fn startup(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Startup is not yet available").await?;
+
+    Ok(())
+}
+
+#[command]
+#[aliases("stop")]
+async fn shutdown(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Shutdown is not yet available").await?;
+
+    Ok(())
 }
